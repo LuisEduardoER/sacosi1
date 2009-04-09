2cb91b15afbfa237412efc74decd0cf628b7bb5b
luizinho16 <luizinho16@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-04-09 22:08:32 +0000

    Monte de coisa feita...fiz de acordo com o que falei com melina...

--- SACO/src/VehicleAlreadyExistsException.java
+++ SACO/src/VehicleAlreadyExistsException.java
@@ -1,6 +1,8 @@
 
 public class VehicleAlreadyExistsException extends Exception{
 
+	private static final long serialVersionUID = -2186727850394433501L;
+
 	public VehicleAlreadyExistsException(String msg) {
 		super(msg);
 	}
