38a5aac8a11f2507145500c75cb787f9f879e9e2
luizinho16 <luizinho16@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-04-09 20:08:06 +0000

    Usuario agora com Login, como tem nos testes... creio que nao falte mais nada, as mensagens de erro estao iguais...

---
+++ SACO/src/LoginException.java
@@ -0,0 +1,9 @@
+
+public class LoginException extends Exception {
+
+	private static final long serialVersionUID = 3698122668479070214L;
+
+	public LoginException(String msg) {
+		super(msg);
+	}
+}
