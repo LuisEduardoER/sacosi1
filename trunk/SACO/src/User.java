c25eab3503cdc98053e10d472f24b7970e4a5e1f
luizinho16 <luizinho16@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-04-09 18:06:15 +0000

    Acho que terminei a classe Usuario. Por favor verifiquem ai q ainda nao tem como rodar os testes =/ nao sei como rodar

--- trunk/SACO/src/User.java
+++ trunk/SACO/src/User.java
@@ -1,19 +1,144 @@
-
 public class User {
-	
+
 	private String name;
-	
 	private String email;
-	
 	private String phone;
-	
-	public User(String name, String email, String phone ) {
-		
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		
+	private static int posicaoInicio = 0;
+	private static int caracteresPhone = 10;
+
+	public User(String name, String email, String phone) {
+
+		this.setName(name);
+		this.setEmail(email);
+		this.setPhone(phone);
+
 	}
-	
-	
+
+	public boolean setName(String name) {
+		if (validName(name)) {
+			this.name = name;
+			return true;
+		} else
+			return false;
+	}
+
+	private boolean isLetterDownCase(char letter) {
+		if (letter < 'a' || letter > 'z')
+			return false;
+		return true;
+	}
+
+	private boolean isLetterUpperCase(char letter) {
+		if (letter < 'A' || letter > 'Z')
+			return false;
+		return true;
+	}
+
+	private boolean isNumber(char number) {
+		if (number < '0' || number > '9')
+			return false;
+		return true;
+	}
+
+	private boolean isPermitedChar(char simbol) {
+		if (simbol != '_' && simbol != '.' && simbol != ',' && simbol != '-')
+			return false;
+		return true;
+	}
+
+	private boolean isSpace(char space) {
+		if (space != ' ')
+			return false;
+		return true;
+	}
+
+	private boolean validName(String name2) {
+		if (name.charAt(posicaoInicio) == '_'
+				|| name.charAt(posicaoInicio) == '.'
+				|| name.charAt(posicaoInicio) == ','
+				|| name.charAt(posicaoInicio) == '-'
+				|| name.charAt(name2.length() - 1) == '_'
+				|| name.charAt(name2.length() - 1) == '.'
+				|| name.charAt(name2.length() - 1) == ','
+				|| name.charAt(name2.length() - 1) == '-') {
+			return false;
+		} else {
+			for (int i = 0; i < name2.length(); i++) {
+				if (!isPermitedChar(name2.charAt(i))
+						&& !isNumber(name2.charAt(i))
+						&& !isLetterDownCase(name2.charAt(i))
+						&& !isLetterUpperCase(name2.charAt(i))
+						&& !isSpace(name2.charAt(i)))
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean setEmail(String email) {
+		if (validEmail(email)) {
+			this.email = email;
+			return true;
+		} else
+			return false;
+	}
+
+	private boolean validEmail(String email2) {
+		int arrobas = 0;
+		if (name.charAt(posicaoInicio) == '_'
+				|| name.charAt(posicaoInicio) == '.'
+				|| name.charAt(posicaoInicio) == ','
+				|| name.charAt(posicaoInicio) == '-'
+				|| name.charAt(email2.length() - 1) == '_'
+				|| name.charAt(email2.length() - 1) == '.'
+				|| name.charAt(email2.length() - 1) == ','
+				|| name.charAt(email2.length() - 1) == '-') {
+			return false;
+		} else {
+			for (int i = 0; i < email2.length(); i++) {
+				if (!isPermitedChar(email2.charAt(i))
+						&& !isNumber(email2.charAt(i))
+						&& !isLetterDownCase(email2.charAt(i))
+						&& !isLetterUpperCase(email2.charAt(i))
+						&& !isSpace(email2.charAt(i)))
+					return false;
+				if (email2.charAt(i) == '@')
+					arrobas++;
+			} 
+			if (arrobas != 1)
+				return false;
+		}
+		return true;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public boolean setPhone(String phone) {
+		if (validPhone(phone)) {
+			this.phone = phone;
+			return true;
+		} else
+			return false;
+	}
+
+	private boolean validPhone(String phone2) {
+		if (phone2.length() != caracteresPhone)
+			return false;
+		for (int i = 0; i < phone2.length(); i++) {
+			if (phone2.charAt(i) < '0' || phone2.charAt(i) > '9')
+				return false;
+		}
+		return true;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
 }
