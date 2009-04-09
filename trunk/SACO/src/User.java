49aa1e0fc063349ae6dbedb6f80dbfbb5c7e6d91
luizinho16 <luizinho16@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-04-09 20:08:06 +0000

    Usuario agora com Login, como tem nos testes... creio que nao falte mais nada, as mensagens de erro estao iguais...

--- trunk/SACO/src/User.java
+++ trunk/SACO/src/User.java
@@ -3,21 +3,33 @@ public class User {
 	private String name;
 	private String email;
 	private String phone;
+	private String login;
 	private static int posicaoInicio = 0;
 	private static int caracteresPhone = 10;
 
-	public User(String name, String email, String phone) throws EmailException,
-			NameException, PhoneException {
+	public User(String name, String email, String phone, String login)
+			throws EmailException, NameException, PhoneException,
+			LoginException {
 
+		if (name.equals(null) || name.equals(""))
+			throw new NameException("error: name is a mandatory field!");
+		if (email.equals(null) || email.equals(""))
+			throw new EmailException("error: e-mail is a mandatory field!");
+		if (phone.equals(null) || phone.equals(""))
+			throw new PhoneException(
+					"error: phone number is a mandatory field!");
+		if (login.equals(null) || login.equals(""))
+			throw new LoginException("error: login is a mandatory field!");
 		this.setName(name);
 		this.setEmail(email);
 		this.setPhone(phone);
+		this.setLogin(login);
 
 	}
 
 	public void setName(String name) throws NameException {
 		if (!validName(name))
-			throw new NameException("Invalid Name");
+			throw new NameException("error: invalid field!");
 		this.name = name;
 	}
 
@@ -52,14 +64,14 @@ public class User {
 	}
 
 	private boolean validName(String name2) {
-		if (name.charAt(posicaoInicio) == '_'
-				|| name.charAt(posicaoInicio) == '.'
-				|| name.charAt(posicaoInicio) == ','
-				|| name.charAt(posicaoInicio) == '-'
-				|| name.charAt(name2.length() - 1) == '_'
-				|| name.charAt(name2.length() - 1) == '.'
-				|| name.charAt(name2.length() - 1) == ','
-				|| name.charAt(name2.length() - 1) == '-') {
+		if (name2.charAt(posicaoInicio) == '_'
+				|| name2.charAt(posicaoInicio) == '.'
+				|| name2.charAt(posicaoInicio) == ','
+				|| name2.charAt(posicaoInicio) == '-'
+				|| name2.charAt(name2.length() - 1) == '_'
+				|| name2.charAt(name2.length() - 1) == '.'
+				|| name2.charAt(name2.length() - 1) == ','
+				|| name2.charAt(name2.length() - 1) == '-') {
 			return false;
 		} else {
 			for (int i = 0; i < name2.length(); i++) {
@@ -80,20 +92,20 @@ public class User {
 
 	public void setEmail(String email) throws EmailException {
 		if (!validEmail(email))
-			throw new EmailException("Invalid Email");
+			throw new EmailException("error: invalid field!");
 		this.email = email;
 	}
 
 	private boolean validEmail(String email2) {
 		int arrobas = 0;
-		if (name.charAt(posicaoInicio) == '_'
-				|| name.charAt(posicaoInicio) == '.'
-				|| name.charAt(posicaoInicio) == ','
-				|| name.charAt(posicaoInicio) == '-'
-				|| name.charAt(email2.length() - 1) == '_'
-				|| name.charAt(email2.length() - 1) == '.'
-				|| name.charAt(email2.length() - 1) == ','
-				|| name.charAt(email2.length() - 1) == '-') {
+		if (email2.charAt(posicaoInicio) == '_'
+				|| email2.charAt(posicaoInicio) == '.'
+				|| email2.charAt(posicaoInicio) == ','
+				|| email2.charAt(posicaoInicio) == '-'
+				|| email2.charAt(email2.length() - 1) == '_'
+				|| email2.charAt(email2.length() - 1) == '.'
+				|| email2.charAt(email2.length() - 1) == ','
+				|| email2.charAt(email2.length() - 1) == '-') {
 			return false;
 		} else {
 			for (int i = 0; i < email2.length(); i++) {
@@ -118,7 +130,7 @@ public class User {
 
 	public void setPhone(String phone) throws PhoneException {
 		if (!validPhone(phone))
-			throw new PhoneException("Invalid Phone");
+			throw new PhoneException("error: invalid field!");
 		this.phone = phone;
 	}
 
@@ -134,6 +146,38 @@ public class User {
 
 	public String getPhone() {
 		return phone;
+	}
+
+	public void setLogin(String login) throws LoginException {
+		if (!validLogin(name))
+			throw new LoginException("error: invalid field!");
+		this.login = login;
+	}
+
+	private boolean validLogin(String login2) {
+		if (login2.charAt(posicaoInicio) == '_'
+				|| login2.charAt(posicaoInicio) == '.'
+				|| login2.charAt(posicaoInicio) == ','
+				|| login2.charAt(posicaoInicio) == '-'
+				|| login2.charAt(login2.length() - 1) == '_'
+				|| login2.charAt(login2.length() - 1) == '.'
+				|| login2.charAt(login2.length() - 1) == ','
+				|| login2.charAt(login2.length() - 1) == '-') {
+			return false;
+		} else {
+			for (int i = 0; i < login2.length(); i++) {
+				if (!isPermitedChar(login2.charAt(i))
+						&& !isNumber(login2.charAt(i))
+						&& !isLetterDownCase(login2.charAt(i))
+						&& !isLetterUpperCase(login2.charAt(i)))
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public String getLogin() {
+		return login;
 	}
 
 }
