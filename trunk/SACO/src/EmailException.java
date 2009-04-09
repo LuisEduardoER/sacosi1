2e607b64d4e155eacd0bad9d52a812a0c5649c7c
luizinho16 <luizinho16@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-04-09 18:58:10 +0000

    Agora com Exceções =D

---
+++ trunk/SACO/src/EmailException.java
@@ -0,0 +1,8 @@
+public class EmailException extends Exception {
+
+	private static final long serialVersionUID = -3575720609773917150L;
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+}
---
+++ trunk/SACO/src/PhoneException.java
@@ -0,0 +1,9 @@
+public class PhoneException extends Exception {
+
+	private static final long serialVersionUID = 1204363382515067974L;
+
+	public PhoneException(String msg) {
+		super(msg);
+	}
+
+}
