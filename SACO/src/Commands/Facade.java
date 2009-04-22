e998ff072998360f137e805cc8c23f4cb6d48e2c
s.costa.filipe <s.costa.filipe@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-04-22 17:08:10 +0000

    fiz um system soh de brincadeira, pra poder mecher um pouco na classe, instanciar objetos, e dah uma olhada no getAllUsers

--- SACO/src/Commands/Facade.java
+++ SACO/src/Commands/Facade.java
@@ -13,6 +13,7 @@ import Exceptions.PriceException;
 import Exceptions.TypeException;
 import Exceptions.YearException;
 import Users.Administrator;
+import Users.User;
 import Vehicles.Car;
 import Vehicles.Motorcycle;
 import Vehicles.Vehicle;
@@ -24,23 +25,6 @@ public class Facade {
 	public static void main(String[] args) {
 		//exemplo de como executar:
 		//EasyAccept.main(new String[] {"Facade", "test1.txt"});
-		try {
-			Administrator admin = new Administrator("foo bar", "foo@bar.com", "6666666666", "acabou"
-									);
-			System.out.println(admin.getLogin());
-		} catch (EmailException e) {
-			// TODO Auto-generated catch block
-			System.err.println(e.getMessage());
-		} catch (NameException e) {
-			// TODO Auto-generated catch block
-			System.err.println(e.getMessage());
-		} catch (PhoneException e) {
-			// TODO Auto-generated catch block
-			System.err.println(e.getMessage());
-		} catch (LoginException e) {
-			// TODO Auto-generated catch block
-			System.err.println(e.getMessage());
-		}
 		
 		
 	}
