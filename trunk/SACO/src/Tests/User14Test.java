9f069eae5a59a416e13ee9db390b148b6f746e50
melmongiovi <melmongiovi@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-04-28 13:45:56 +0000

    Teste 14 passando...

---
+++ trunk/SACO/src/Tests/User14Test.java
@@ -0,0 +1,35 @@
+package Tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Commands.Facade;
+import easyaccept.EasyAcceptFacade;
+
+public class User14Test {
+	public static void main(String[] args) throws Exception {
+
+		List<String> files = new ArrayList<String>();
+
+		//Put the us1.txt file into the "test scripts" list
+
+		files.add("us14.txt");
+
+		//Instantiate the Monopoly Game façade
+
+		Facade test = new Facade();
+
+		//Instantiate EasyAccept façade
+
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(test, files);
+
+		//Execute the tests
+
+		eaFacade.executeTests();
+
+		//Print the tests execution results
+
+		System.out.println(eaFacade.getCompleteResults());
+
+	}
+}
