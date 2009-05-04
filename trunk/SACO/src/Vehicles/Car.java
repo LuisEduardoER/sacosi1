bb517a59440c69026265ada750a9d464fd8d981d
melmongiovi <melmongiovi@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-05-04 02:44:07 +0000

    Javadoc do pacote vehicles pronto

--- trunk/SACO/src/Vehicles/Car.java
+++ trunk/SACO/src/Vehicles/Car.java
@@ -10,8 +10,34 @@ import Exceptions.TypeException;
 import Exceptions.YearException;
 import Exceptions.ModelException;
 
+/**
+ * Esta classe representa um carro no sistema
+ * @author Filipe
+ * @author Luiz
+ * @author Melina
+ * @author Raissa
+ * @author Ramon
+ *
+ */
 public class Car extends Vehicle{
-
+	
+	/**
+	 * Contrutor
+	 * @param type
+	 * @param model
+	 * @param color
+	 * @param plate
+	 * @param year
+	 * @param price
+	 * @throws InvalidFieldException
+	 * @throws NoFieldException
+	 * @throws TypeException
+	 * @throws ModelException
+	 * @throws ColorException
+	 * @throws PlateException
+	 * @throws PriceException
+	 * @throws YearException
+	 */
 	public Car(String type, String model, String color, String plate, String year,
 			String price) throws InvalidFieldException, NoFieldException, TypeException, ModelException, 
 						  ColorException, PlateException, PriceException, YearException {
--- trunk/SACO/src/Vehicles/Vehicle.java
+++ trunk/SACO/src/Vehicles/Vehicle.java
@@ -1,8 +1,21 @@
 package Vehicles;
 
 
+/**
+ * Esta classe representa um veiculo no sistema
+ * @author Filipe
+ * @author Luiz
+ * @author Melina
+ * @author Raissa
+ * @author Ramon
+ *
+ */
 public abstract class Vehicle implements Alugavel {
+	
+	private int year;
 
+	private Double price;
+	
 	private String type;
 
 	private String model;
@@ -10,14 +23,18 @@ public abstract class Vehicle implements Alugavel {
 	private String color;
 
 	private String plate;
-
-	private int year;
-
-	private Double price;
-
+	
+	/**
+	 * Contrutor
+	 * @param type
+	 * @param model
+	 * @param color
+	 * @param plate
+	 * @param year
+	 * @param price
+	 */
 	public Vehicle(String type, String model, String color, String plate,
 			String year, String price) {
-
 		this.setType(type);
 		this.setYear(Integer.parseInt(year));
 		this.setModel(model);
@@ -25,53 +42,93 @@ public abstract class Vehicle implements Alugavel {
 		this.setPlate(plate);
 		this.setPrice(Double.parseDouble(price));
 	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	public void setPlate(String plate) {
-		this.plate = plate;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-	public String getModel() {
-		return model;
-	}
-
-	public String getPlate() {
-		return plate;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
+	/**
+	 * @return the type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the model
+	 */
+	public String getModel() {
+		return model;
+	}
+
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the plate
+	 */
+	public String getPlate() {
+		return plate;
+	}
+
+	/**
+	 * @param plate the plate to set
+	 */
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+	/**
+	 * @return the year
+	 */
 	public int getYear() {
 		return year;
 	}
+
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public Double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+
+
+
 
 }
