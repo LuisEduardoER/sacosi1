878355ae40fa71ea177f3f32fd73a59898c075a1
melmongiovi <melmongiovi@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-05-04 02:44:07 +0000

    Javadoc do pacote vehicles pronto

--- SACO/src/Vehicles/Motorcycle.java
+++ SACO/src/Vehicles/Motorcycle.java
@@ -9,8 +9,34 @@ import Exceptions.TypeException;
 import Exceptions.YearException;
 import Exceptions.ModelException;
 
+/**
+ * Esta classe representa uma moto no sistema
+ * @author Filipe
+ * @author Luiz
+ * @author Melina
+ * @author Raissa
+ * @author Ramon
+ *
+ */
 public class Motorcycle extends Vehicle{
 
+	/**
+	 * Construtor
+	 * @param type
+	 * @param model
+	 * @param color
+	 * @param plate
+	 * @param year
+	 * @param price
+	 * @throws InvalidFieldException
+	 * @throws NoFieldException
+	 * @throws TypeException
+	 * @throws ModelException
+	 * @throws ColorException
+	 * @throws PlateException
+	 * @throws PriceException
+	 * @throws YearException
+	 */
 	public Motorcycle(String type, String model, String color, String plate,
 			String year, String price) throws InvalidFieldException, NoFieldException, TypeException, 
 									ModelException, ColorException, PlateException, PriceException, YearException {
