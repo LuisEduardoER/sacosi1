429ab3e8d2d42afb52404f3975a4ca7fd91a8194
melmongiovi <melmongiovi@b270f4d2-237b-11de-96a1-5542d1fc8aa6>
2009-05-05 18:20:01 +0000

    Java doc pronto

--- trunk/SACO/src/LogicalSystem/LogicOfTheAdmin.java
+++ trunk/SACO/src/LogicalSystem/LogicOfTheAdmin.java
@@ -27,6 +27,7 @@ import Exceptions.UserNotFoundException;
 import Exceptions.YearException;
 
 /**
+ * Logica do administrador
  * @author Filipe
  * @author Melina
  * @author Luiz
@@ -38,30 +39,98 @@ import Exceptions.YearException;
 public class LogicOfTheAdmin extends LogicOfTheUser {
 	private Facade facade;
 	
+	/**
+	 * Construtor
+	 * @throws Exception
+	 */
 	public LogicOfTheAdmin() throws Exception {
 		this.facade = new Facade();
 	}
 	
+	/**
+	 * Adiciona usuario
+	 * @param login
+	 * @param name
+	 * @param email
+	 * @param phone
+	 * @throws InvalidLoginException
+	 * @throws EmailException
+	 * @throws InvalidNameException
+	 * @throws PhoneException
+	 * @throws InvalidFieldException
+	 * @throws UserAlreadyExistException
+	 */
 	public void addUser(String login, String name, String email, String phone) throws InvalidLoginException, EmailException, InvalidNameException, PhoneException, InvalidFieldException, UserAlreadyExistException{  
 		facade.addUser(login, name, email, phone);
 	}
 	
+	/**
+	 * Adiciona cliente
+	 * @param name
+	 * @param email
+	 * @param phone
+	 * @throws EmailException
+	 * @throws InvalidNameException
+	 * @throws PhoneException
+	 * @throws CustomerAlreadyExistException
+	 * @throws InvalidFieldException
+	 */
 	public void addCustomer(String name, String email, String phone) throws EmailException, InvalidNameException, PhoneException, CustomerAlreadyExistException, InvalidFieldException{
 		facade.addCustomer(name, email, phone);
 	}
 	
+	/**
+	 * Remove cliente
+	 * @param email
+	 * @throws ClientNotRegisteredException
+	 * @throws NoCustomerOnDatabaseException
+	 * @throws InvalidParameterException
+	 */
 	public void removeCustomer(String email) throws ClientNotRegisteredException, NoCustomerOnDatabaseException, InvalidParameterException{
 		facade.removeCustomer(email);
 	}
 	
+	/**
+	 * Remove usuario
+	 * @param emailOrLogin
+	 * @throws LoginException
+	 * @throws UserNotFoundException
+	 * @throws EmailException
+	 * @throws NoUserOnDatabaseException
+	 * @throws InvalidParameterException
+	 */
 	public void removeUser(String emailOrLogin) throws LoginException, UserNotFoundException, EmailException, NoUserOnDatabaseException, InvalidParameterException{
 		facade.removeUser(emailOrLogin);
 	}
 	
+	/**
+	 * adiciona veiculo
+	 * @param type
+	 * @param model
+	 * @param color
+	 * @param year
+	 * @param plate
+	 * @param price
+	 * @throws InvalidFieldException
+	 * @throws NoFieldException
+	 * @throws TypeException
+	 * @throws ModelException
+	 * @throws ColorException
+	 * @throws PlateException
+	 * @throws PriceException
+	 * @throws YearException
+	 * @throws PlateAlreadyExistsException
+	 */
 	public void addVehicle(String type, String model, String color, String year, String plate, String price) throws InvalidFieldException, NoFieldException, TypeException, ModelException, ColorException, PlateException, PriceException, YearException, PlateAlreadyExistsException{
 		facade.addVehicle(type, model, color, plate, year, price);
 	}
 	
+	/**
+	 * remove veiculo
+	 * @param plate
+	 * @throws NoSuchVehicleException
+	 * @throws NoVehicleOnDatabaseException
+	 */
 	public void removeVehicle(String plate) throws NoSuchVehicleException, NoVehicleOnDatabaseException{
 		facade.removeVehicle(plate);
 	}
--- trunk/SACO/src/LogicalSystem/LogicOfTheUser.java
+++ trunk/SACO/src/LogicalSystem/LogicOfTheUser.java
@@ -13,7 +13,7 @@ import Exceptions.PhoneException;
 import Users.Alugadores;
 
 /**
- * 
+ * Logica do usuario
  * @author Filipe
  * @author Melina
  * @author Luiz
@@ -25,50 +25,125 @@ public class LogicOfTheUser {
 	
 	private Facade facade;
 	
+	/**
+	 * Construtor
+	 * @throws Exception
+	 */
 	public LogicOfTheUser() throws Exception{
 		this.facade = new Facade();
 	}
 	
+	/**
+	 * Visualiza requisições
+	 */
 	public void seeRequisitions(){
 		facade.listAllRequests();
 	}
 	
+	/**
+	 * Adiciona aluguel
+	 * @param email
+	 * @param plate
+	 * @param initialDate
+	 * @param finalDate
+	 * @throws InvalidParameterException
+	 * @throws InvalidDateException
+	 * @throws EmailException
+	 * @throws InvalidNameException
+	 * @throws PhoneException
+	 * @throws CustomerAlreadyExistException
+	 * @throws InvalidFieldException
+	 */
 	public void addRent(String email, String plate, String initialDate, String finalDate) throws InvalidParameterException, InvalidDateException, EmailException, InvalidNameException, PhoneException, CustomerAlreadyExistException, InvalidFieldException{
 		facade.registerRent(plate, email, initialDate, finalDate);
 	}
 	
+	/**
+	 * Adiciona varios alugueis
+	 * @param customer
+	 * @param plates
+	 * @param initialDates
+	 * @param devolutionDates
+	 * @throws InvalidParameterException
+	 * @throws InvalidDateException
+	 * @throws EmailException
+	 * @throws InvalidNameException
+	 * @throws PhoneException
+	 * @throws CustomerAlreadyExistException
+	 * @throws InvalidFieldException
+	 */
 	public void addManyRents(Alugadores customer, String[] plates, String[] initialDates, String[] devolutionDates) throws InvalidParameterException, InvalidDateException, EmailException, InvalidNameException, PhoneException, CustomerAlreadyExistException, InvalidFieldException{
 		facade.addManyRents(customer, plates, initialDates, devolutionDates);
 	}
 	
+	/**
+	 * Libera aluguel
+	 * @param plate
+	 */
 	public void releaseRent(String plate){
 		facade.releaseVehicle(plate);
 	}
 	
+	/**
+	 * Notifica liberação de aluguel
+	 * @param plate
+	 */
 	public void notifyRelease(String plate){
 		System.out.println("Email com confirmacao de liberacao enviado!");
 	}
 	
+	/**
+	 * Registra aluguel atrasado
+	 * @param email
+	 * @param plate
+	 * @param initialDate
+	 * @param finalDate
+	 * @throws InvalidDateException
+	 * @throws InvalidParameterException
+	 * @throws EmailException
+	 * @throws InvalidNameException
+	 * @throws PhoneException
+	 * @throws CustomerAlreadyExistException
+	 * @throws InvalidFieldException
+	 */
 	public void registerLateRent(String email, String plate, String initialDate, String finalDate) throws InvalidDateException, InvalidParameterException, EmailException, InvalidNameException, PhoneException, CustomerAlreadyExistException, InvalidFieldException{
 		facade.registerLateRent(plate, email, initialDate, finalDate);
 	}
 	
+	/**
+	 * Notifica aluguel atrasado
+	 */
 	public void notifyLateRent(){
 		System.out.println("Email com notificacao sobre atraso no aluguel");
 	}
 	
+	/**
+	 * obtem situação do veiculo
+	 * @param plate
+	 */
 	public void getVehicleSituation(String plate){
 		facade.getVehicleSituation(plate);
 	}
 	
+	/**
+	 * obtem situação de todos os veiculos
+	 */
 	public void getAllVehiclesSituation(){
 		facade.getAllVehiclesSituation();
 	}
 	
+	/**
+	 * Visualiza aluguel corrente
+	 * @param date
+	 */
 	public void seeCurrentRent(Calendar date){
 		facade.listAllNonPendingRents(date);
 	}
 	
+	/**
+	 * Visualiza aluguel atrasado
+	 * @param date
+	 */
 	public void seeLateRent(Calendar date){
 		facade.listAllPendingRents(date);
 	}
